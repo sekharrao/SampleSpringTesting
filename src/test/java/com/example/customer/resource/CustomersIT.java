bbe0a2398b5fe9fc16677d79fac14f776ae9009f
sekharrao <m.sekhar.rao@gmail.com>
2019-11-14 11:19:32 +0000

    updated test case

--- src/test/java/com/example/customer/resource/CustomersIT.java
+++ src/test/java/com/example/customer/resource/CustomersIT.java
@@ -8,20 +8,19 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import com.example.customer.Application;
 import com.example.customer.model.Customer;
 import com.example.customer.repo.CustomerRepo;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK, classes= Application.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @AutoConfigureMockMvc
-@TestPropertySource(locations = "classpath:app-integrationtest.properties")
+@AutoConfigureTestDatabase
 public class CustomersIT {
 
 	@Autowired
